a9a47672243478230c6cfc9864fdf4bc080abc71
mondlichthari <mrzmr999@163.com>
2026-05-25 16:12:18 +0800

    first commit

---
+++ test.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a,b,c;
+    cout<<"thinkdifferent"<<endl;
+    cin>>a>>b;
+    c=a+b;
+    cout<<a<<' '<<b<<' '<<c<<endl;
+    return 0;
+}
